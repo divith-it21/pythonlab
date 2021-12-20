a4690a0fb26fe01c3490f6472da6de5355b61e4d
divith-it21 <divith.it21@bitsathy.ac.in>
2021-12-20 03:47:46 +0000

    commitmsg

--- ifblock.py
+++ ifblock.py
@@ -1,6 +1,8 @@
 a=int(input("Enter a num"))
-if a>=10:
-print(a,"is greater than or equal to 10")
+if a>10:
+print(a,"is greater than 10")
+elif a=10:
+print(a,"equal to 10")
 else:
 print(a,"is less than 10")
 
